535f2c53e1b21f92b761fe191d0d113f7df10f83
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2025-01-25 08:35:34 +0900

    displaying the sign in and sign out buttons

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,36 @@
-import { auth } from '@/auth';
 import Link from 'next/link';
+import Image from 'next/image';
+import { auth } from '@/auth';
 import { Input } from './ui/input';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import { Button } from './ui/button';
 
 export default async function Header() {
     const session = await auth();
+
+    let authContent: React.ReactNode;
+    if (session?.user) {
+        console.log(session.user);
+        authContent = (
+            <div>
+                <Avatar>
+                    {session.user.image && (
+                        <AvatarImage src={session.user.image} />
+                    )}
+                    <AvatarFallback>{session.user.name}</AvatarFallback>
+                </Avatar>
+            </div>
+        );
+    } else {
+        authContent = (
+            <div className="flex gap-3">
+                <Button type="submit" className="bg-zinc-500">
+                    Sign in
+                </Button>
+                <Button type="submit">Sign up</Button>
+            </div>
+        );
+    }
     return (
         <header className="border">
             <nav className="flex justify-between items-center">
@@ -15,13 +42,7 @@ export default async function Header() {
                 <div className="px-16 py-3">
                     <Input />
                 </div>
-                <div className="px-16 py-3">
-                    {session?.user ? (
-                        <div>signed out</div>
-                    ) : (
-                        <div>signed in</div>
-                    )}
-                </div>
+                <div className="px-16 py-3">{authContent}</div>
             </nav>
         </header>
     );
