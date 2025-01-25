1a37ef79e7a1debfc42e332d9f42be9f23f643e2
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2025-01-25 09:54:59 +0900

    creating a popover form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import TopicCreateForm from '@/components/topic/topic-create-form';
 
 export default async function Home() {
     return (
-        <div className="grid grid-cols-4 gap-4 p-4">
+        <div className="grid grid-cols-4 gap-4 p-4 items-center">
             <div className="col-span-3">
                 <h1 className="text-xl m-2">Top Posts</h1>
             </div>
-            <div>
+            <div className="mx-auto">
                 <TopicCreateForm />
             </div>
         </div>
--- src/components/topic/topic-create-form.tsx
+++ src/components/topic/topic-create-form.tsx
@@ -1,3 +1,37 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from '@/components/ui/popover';
+import { Textarea } from '../ui/textarea';
+import { createTopic } from '@/actions';
+
 export default function TopicCreateForm() {
-    return <div>Topic Create Form </div>;
+    return (
+        <Popover>
+            <PopoverTrigger asChild>
+                <Button className="bg-blue-600 hover:bg-blue-500">
+                    Create a Topic
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-80">
+                <form action={createTopic}>
+                    <div className="flex flex-col gap-4 p-4">
+                        <h3 className="text-lg">Create a Topic</h3>
+                        <Label htmlFor="name">Name</Label>
+                        <Input id="name" placeholder="Name" />
+                        <Label htmlFor="description">Description</Label>
+                        <Textarea
+                            id="description"
+                            placeholder="Describe your topic"
+                        />
+                        <Button type="submit">Submit</Button>
+                    </div>
+                </form>
+            </PopoverContent>
+        </Popover>
+    );
 }
