d9a5b6d677166d6cc02284dfd546d1657769da3d
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2025-01-25 10:10:17 +0900

    adding form validation with zod

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,6 +1,24 @@
 'use server';
 
+import { z } from 'zod';
+
+const CreateTopicSchema = z.object({
+    name: z
+        .string()
+        .min(3)
+        .regex(/^[a-z-]+$/, {
+            message: 'Must be lowercase letters or dashes without spaces',
+        }),
+    description: z.string().min(10),
+});
+
 export const createTopic = async (formData: FormData) => {
-    const name = formData.get('name');
-    const description = formData.get('description');
+    const result = CreateTopicSchema.safeParse({
+        name: formData.get('name'),
+        description: formData.get('description'),
+    });
+
+    if (!result.success) {
+        console.log(result.error.flatten().fieldErrors);
+    }
 };
