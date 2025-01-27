6dcffa8ff02a5ad1259936f8cb552a8644b2d1c1
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2025-01-27 11:55:33 +0900

    scaffolding the topic show page

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,3 +1,14 @@
-export default function TopicPage() {
-    return <div>TopicPage</div>;
+type Props = {
+    params: Promise<{ slug: string }>;
+};
+
+export default async function TopicShowPage({ params }: Props) {
+    const { slug } = await params;
+    return (
+        <div className="grid grid-cols-4 gap-4 p-4">
+            <div className="col-span-3">
+                <h1 className="text-2xl font-bold mb-2">{slug}</h1>
+            </div>
+        </div>
+    );
 }
