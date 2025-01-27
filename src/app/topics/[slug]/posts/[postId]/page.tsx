2f957311a8cc11c9ff48fe62b289adb460cc3437
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2025-01-27 12:59:19 +0900

    mergin a few files

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,3 +1,30 @@
-export default function PostShowPage() {
-    return <div>PostShowPage</div>;
+import Link from 'next/link';
+import PostShow from '@/components/posts/post-show';
+import CommentList from '@/components/comments/comment-list';
+import CommentCreateForm from '@/components/comments/comment-create-form';
+import { path } from '@/path';
+
+interface PostShowPageProps {
+    params: Promise<{
+        slug: string;
+        postId: string;
+    }>;
+}
+
+export default async function PostShowPage({ params }: PostShowPageProps) {
+    const { slug, postId } = await params;
+
+    return (
+        <div className="space-y-3">
+            <Link
+                className="underline decoration-solid"
+                href={path.topicShow({ topicSlug: slug })}
+            >
+                {'< '}Back to {slug}
+            </Link>
+            {/* <PostShow /> */}
+            {/* <CommentCreateForm postId={postId} startOpen /> */}
+            {/* <CommentList comments={comments} /> */}
+        </div>
+    );
 }
